917bb1e624842e956f66918bbb63cd10210a084c
Emanuel Burgos <emaburgos17@hotmail.com>
2023-07-13 21:48:14 -0300

    Backd 21

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
 import conectarDB from "./config/db.js";
 import veterinarioRoutes from "./routes/veterinarioRoutes.js";
 import pacienteRoutes from "./routes/pacienteRoutes.js";
+import cors from "cors";
 
 const app = express();
 
@@ -13,19 +13,7 @@ dotenv.config();
 
 conectarDB();
 
-const dominiosPermitidos = process.env.FRONT_URL;
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (dominiosPermitidos.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("No permitido por CORS"));
-    }
-  },
-};
-
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api/veterinarios", veterinarioRoutes);
 app.use("/api/pacientes", pacienteRoutes);
