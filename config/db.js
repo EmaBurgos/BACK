62a52fbde46bbbe56e07894e9eb98a33794e02d9
Emanuel Burgos <emaburgos17@hotmail.com>
2023-07-13 22:00:58 -0300

    m

--- config/db.js
+++ config/db.js
@@ -2,10 +2,13 @@ import mongoose from "mongoose";
 
 const conectarDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const db = await mongoose.connect(
+      "mongodb+srv://ema735959:T5DimzYz85hNXGH3@cluster0.nswf7lu.mongodb.net/apv?retryWrites=true&w=majority",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     const url = `${db.connection.host}:${db.connection.port}`;
     console.log(`Mongo DB conectado en : ${url}`);
   } catch (error) {
